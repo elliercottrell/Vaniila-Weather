6566aca40fe9c3168b3d379dce4ca0bb988eca52
elliercottrell <85800129+elliercottrell@users.noreply.github.com>
2021-10-31 16:48:05 +0000

    Showing current weather details

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let feelsElement = document.querySelector("#feels-like");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°`;
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  feelsElement.innerHTML = `${Math.round(response.data.main.feels_like)}°`;
+  humidityElement.innerHTML = `${response.data.main.humidity} %`;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "5da7b2dc058f07286fea39c4cee516a3";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
