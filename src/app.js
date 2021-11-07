d7dc733090c6d35a40e951cfa0f118139c69ed3f
elliercottrell <85800129+elliercottrell@users.noreply.github.com>
2021-11-07 14:57:34 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -64,7 +64,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "5da7b2dc058f07286fea39c4cee516a3";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
